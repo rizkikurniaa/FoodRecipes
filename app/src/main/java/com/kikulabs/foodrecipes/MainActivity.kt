a453ec4133f2e9566435a1babfb8b8cc5266316f
Rizki <rizkikurniawan1797@gmail.com>
2020-12-28 16:37:36 +0700

    Updated

--- app/src/main/java/com/kikulabs/foodrecipes/MainActivity.kt
+++ app/src/main/java/com/kikulabs/foodrecipes/MainActivity.kt
@@ -1,11 +1,107 @@
 package com.kikulabs.foodrecipes
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AutoCompleteTextView
+import android.widget.LinearLayout
+import android.widget.SearchView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kikulabs.foodrecipes.adapter.ListRecommendationAdapter
+import com.kikulabs.foodrecipes.databinding.ActivityMainBinding
+import com.kikulabs.foodrecipes.viewModel.HomeViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var listRecommendationAdapter: ListRecommendationAdapter
+    private lateinit var homeViewModel: HomeViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
+        initListener()
+        initViewModel()
     }
+
+    private fun initView() {
+
+        val myColor: Int = Color.parseColor("#1B8045")
+        binding.swLayout.setColorSchemeColors(myColor)
+
+        binding.toolbarHome.collapseToolbar.setExpandedTitleColor(Color.argb(0, 0, 0, 0))
+        binding.toolbarHome.collapseToolbar.setContentScrimColor(resources.getColor(R.color.white))
+
+        val searchPlateId: Int = binding.toolbarHome.searchRecipe.context.resources
+            .getIdentifier("android:id/search_plate", null, null)
+        val searchPlate: View =
+            binding.toolbarHome.searchRecipe.findViewById(searchPlateId)
+        searchPlate.setBackgroundColor(Color.TRANSPARENT)
+
+        val linearLayout1 = binding.toolbarHome.searchRecipe.getChildAt(0) as LinearLayout
+        val linearLayout2 = linearLayout1.getChildAt(2) as LinearLayout
+        val linearLayout3 = linearLayout2.getChildAt(1) as LinearLayout
+        val autoComplete = linearLayout3.getChildAt(0) as AutoCompleteTextView
+        autoComplete.textSize = 14f
+
+        binding.toolbarHome.searchRecipe.setOnQueryTextListener(this)
+        binding.toolbarHome.searchRecipe.isFocusable = false
+
+        binding.rvRecommendation.setHasFixedSize(true)
+
+        listRecommendationAdapter = ListRecommendationAdapter()
+        listRecommendationAdapter.notifyDataSetChanged()
+
+        binding.rvRecommendation.layoutManager = LinearLayoutManager(this)
+        binding.rvRecommendation.adapter = listRecommendationAdapter
+
+    }
+
+    private fun initViewModel() {
+        homeViewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.NewInstanceFactory()
+        ).get(HomeViewModel::class.java)
+
+        homeViewModel.getRecommendation().observe(this, Observer { recommendItems ->
+            if (recommendItems.isNotEmpty()) {
+                listRecommendationAdapter.setData(recommendItems)
+                showLoading(false)
+            } else {
+                showLoading(false)
+            }
+        })
+
+    }
+
+    private fun initListener() {
+
+        binding.swLayout.post {
+            showLoading(true)
+            homeViewModel.setRecommendation()
+        }
+
+        binding.swLayout.setOnRefreshListener {
+            showLoading(true)
+            homeViewModel.setRecommendation()
+        }
+
+    }
+
+    private fun showLoading(state: Boolean) {
+        binding.swLayout.isRefreshing = state
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        TODO("Not yet implemented")
+    }
+
 }
