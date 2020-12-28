26b7d455003f0bb4eb0650513cafcc630be51ce3
Rizki <rizkikurniawan1797@gmail.com>
2020-12-28 16:35:16 +0700

    Added data categories

---
+++ app/src/main/java/com/kikulabs/foodrecipes/model/DataCategories.kt
@@ -0,0 +1,7 @@
+package com.kikulabs.foodrecipes.model
+
+data class DataCategories (
+    var strCategory: String? = null,
+    var strCategoryThumb: String? = null,
+    var strCategoryDescription: String? = null
+)
