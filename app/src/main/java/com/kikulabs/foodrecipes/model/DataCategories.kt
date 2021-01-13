936318d8a5e614dfd6c87d82802a05f9cd18dda4
Rizki <rizkikurniawan1797@gmail.com>
2021-01-13 22:03:38 +0700

    Added parceable to categories

--- app/src/main/java/com/kikulabs/foodrecipes/model/DataCategories.kt
+++ app/src/main/java/com/kikulabs/foodrecipes/model/DataCategories.kt
@@ -1,7 +1,37 @@
 package com.kikulabs.foodrecipes.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class DataCategories (
     var strCategory: String? = null,
     var strCategoryThumb: String? = null,
     var strCategoryDescription: String? = null
-)
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(strCategory)
+        parcel.writeString(strCategoryThumb)
+        parcel.writeString(strCategoryDescription)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<DataCategories> {
+        override fun createFromParcel(parcel: Parcel): DataCategories {
+            return DataCategories(parcel)
+        }
+
+        override fun newArray(size: Int): Array<DataCategories?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
